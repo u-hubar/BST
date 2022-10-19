c47e1577602d39e6f628c303e82e60afcba412e5
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2022-10-19 11:20:59 +0200

    Removed external getters, fixed data types and push functions

--- contracts/BlockchainShardingTable.sol
+++ contracts/BlockchainShardingTable.sol
@@ -11,14 +11,15 @@ contract BlockchainShardingTable {
     struct Peer {
         string prevPeer;
         string nextPeer;
+        string id;
         uint32 stake;
         uint8 ask;
     }
 
-    string public emptyPointer;
+    string private emptyPointer;
     string public head;
     string public tail;
-    uint256 public peerCounter;
+    uint16 public peerCounter;
 
     mapping(string => Peer) peers;
 
@@ -29,41 +30,17 @@ contract BlockchainShardingTable {
         peerCounter = 0;
     }
 
-    function getTableSize()
-        external
-        view
-        returns (uint256)
-    {
-        return peerCounter;
-    }
-
-    function getHeadId()
-        external
-        view
-        returns (string memory)
-    {
-        return head;
-    }
-
-    function getTailId()
-        external
-        view
-        returns (string memory)
-    {
-        return tail;
-    }
-
     function getPeer(string calldata peerId)
         external
         view
-        returns (string memory, string memory, uint32, uint8)
+        returns (string memory, uint32, uint8)
     {
         Peer memory peer = peers[peerId];
 
-        return (peer.prevPeer, peer.nextPeer, peer.stake, peer.ask);
+        return (peer.id, peer.stake, peer.ask);
     }
 
-    function getShardingTable(string memory startingPeerId, uint256 nodesNumber)
+    function getShardingTable(string memory startingPeerId, uint16 nodesNumber)
         public
         view
         returns (Peer[] memory)
@@ -73,7 +50,7 @@ contract BlockchainShardingTable {
         Peer[] memory peersPage = new Peer[](nodesNumber);
 
         peersPage[0] = peers[startingPeerId];
-        uint256 i = 1;
+        uint16 i = 1;
 
         while (i < nodesNumber && !_equalIds(peersPage[i-1].nextPeer, emptyPointer)) {
             peersPage[i] = peers[peersPage[i-1].nextPeer];
@@ -95,23 +72,23 @@ contract BlockchainShardingTable {
     {
         _createPeer(peerId, stake, ask);
 
-        if (_equalIds(tail, emptyPointer)) tail = peerId;
-        else _link(tail, peerId);
+        if (!_equalIds(tail, emptyPointer)) _link(tail, peerId);
+        _setTail(peerId);
 
-        if (_equalIds(head, emptyPointer)) head = peerId;
+        if (_equalIds(head, emptyPointer)) _setHead(peerId);
 
         peerCounter += 1;
     }
 
-    function pushFront(string memory peerId, uint8 stake, uint8 ask)
+    function pushFront(string memory peerId, uint32 stake, uint8 ask)
         public
     {
         _createPeer(peerId, stake, ask);
 
-        if (_equalIds(head, emptyPointer)) head = peerId;
-        else _link(peerId, head);
+        if (!_equalIds(head, emptyPointer)) _link(peerId, head);
+        _setHead(peerId);
 
-        if (_equalIds(tail, emptyPointer)) tail = peerId;
+        if (_equalIds(tail, emptyPointer)) _setTail(peerId);
 
         peerCounter += 1;
     }
@@ -126,11 +103,11 @@ contract BlockchainShardingTable {
             _setTail(emptyPointer);
         }
         else if (_equalIds(head, peerId)) {
-            head = removedPeer.nextPeer;
+            _setHead(removedPeer.nextPeer);
             peers[head].prevPeer = emptyPointer;
         }
         else if (_equalIds(tail, peerId)) {
-            tail = removedPeer.prevPeer;
+            _setTail(removedPeer.prevPeer);
             peers[tail].nextPeer = emptyPointer;
         }
         else {
@@ -161,6 +138,7 @@ contract BlockchainShardingTable {
         Peer memory newPeer = Peer(
             emptyPointer,
             emptyPointer,
+            peerId,
             stake,
             ask
         );
